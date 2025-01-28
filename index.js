97099de2d788fed09eee189d08b3fb68dde645e5
abusayedrifat <133402066+abusayedrifat@users.noreply.github.com>
2025-01-28 15:19:49 +0600

    userDB successfully created

--- index.js
+++ index.js
@@ -4,15 +4,17 @@ const app = express()
 const cors = require('cors')
 const port = process.env.PORT || 5000;
 
+app.use(express.json())
 app.use(cors())
-app.use(express())
 
 app.get('/',(req,res)=>{
     res.send('arts and crafts server is running')
 })
 
 const { MongoClient, ServerApiVersion } = require('mongodb');
-const uri = `mongodb+srv://${process.env.DB_user}:${process.env.DB_Pass}@cluster0.faxnq.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0`;
+
+// const uri = "mongodb://localhost:27017/"
+const uri = `mongodb+srv://${process.env.DB_User}:${process.env.DB_Pass}@cluster0.faxnq.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0`;
 
 // Create a MongoClient with a MongoClientOptions object to set the Stable API version
 const client = new MongoClient(uri, {
@@ -26,13 +28,29 @@ const client = new MongoClient(uri, {
 async function run() {
   try {
     // Connect the client to the server	(optional starting in v4.7)
-    await client.connect();
+    // await client.connect();
+
+    const craftsCollection = client.db('craftsDB').collection('art&crafts')
+    const userColllection  = client.db('craftsUserDB').collection('users')
+     
+    app.post('/users',async(req,res)=>{
+        const users = req.body
+        console.log(users);
+        
+        const result = await userColllection.insertOne(users)
+        res.send(result)
+    })
+    app.get('/users',async(req,res)=>{
+        const cursor = userColllection.find()
+        const result = await cursor.toArray();
+        res.send(result)
+    })
     // Send a ping to confirm a successful connection
-    await client.db("admin").command({ ping: 1 });
+    // await client.db("admin").command({ ping: 1 });
     console.log("Pinged your deployment. You successfully connected to MongoDB!");
   } finally {
     // Ensures that the client will close when you finish/error
-    await client.close();
+    // await client.close();
   }
 }
 run().catch(console.dir);
